8e7ad5f8ab16fe7aac18d65555238efbb6360f8c
libin <libin@3ti.us>
2016-01-04 20:44:29 +0800

    订单详情、列表、拆分初始化的接口开发

--- Code/admin/admin/OrderInfoModel.php
+++ Code/admin/admin/OrderInfoModel.php
@@ -40,20 +40,23 @@ require(dirname(__FILE__) . '/includes/init.php');
 			}elseif ($this->command == 'page'){
 				//
 				$this->pageAction();
+			}elseif ($this->command == 'splitInit'){
+				//
+				$this->splitInitAction();
 			}
 		}
 		
 		/**
 		 * 接口名称：订单详情
-		 * 接口地址：http://admin.zjgit.dev/admin/OrderInfoModel.php
+		 * 接口地址：http://admin.zj.dev/admin/OrderInfoModel.php
 		 * 请求方法：POST
 		 * 传入的接口数据格式如下(具体参数在parameters下的params， "where"可以为空，有则 表示搜索条件，"limit"表示页面首条记录所在行数, "offset"表示要显示的数量)：
 	     *  {
-	     *      "entity": 'order_info',
-	     *      "command": 'page',
+	     *      "entity": "order_info",
+	     *      "command": "find",
 	     *      "parameters": {
 	     *          "params": {
-	     *          	"order_id":1//订单ID
+	     *          	"order_id":101//订单ID
 	     *          }
 	     *      }
 	     *  }
@@ -64,16 +67,37 @@ require(dirname(__FILE__) . '/includes/init.php');
 		 *	    "content": { 
 		 *	    	"info":
 		 *	        {
-		 *	        	
+		 *	        	"order_id":1,//订单编号
+		 *	        	"order_status":0,//0 新增 1 已完成 2处理中 3已撤单
+		 *	        	"order_sn":"os122311",//订单编号
+		 *	        	"user_id":1,//客户编号 
+		 *	        	"user_name":"王x",//客户名称
+		 *	        	"add_time":"2015-01-01:11",//下单时间
+		 *	        	"contract_sn":"10000",//销售合同号
+		 *	        	"contract_name":"钢材销售",//销售合同名称
+		 *	        	"consignee":"aa",//收货人
+		 *	        	"address":"xx地址",//收货地址
+		 *	        	"mobile":"13011111111",//手机号码
+		 *	        	"sign_building":"xx桥"//标志性建筑
 		 *	        },
 		 *	        "invoice":
 		 *	        {
-		 *	        	
+		 *	        	"inv_type":0,//发票类型 0 增值税专用 1 普通发票
+		 *	        	"inv_payee":"xx公司",//发票抬头
 		 *	        },
 		 *	        "goods":
 		 *	        [
 		 *	        {
-		 *	        	
+		 *	        	"goods_sn":"g0001",//物料代码
+		 *	        	"goods_name":"螺纹hb400",//名称
+		 *	        	"attr":"45/T450/gangjin",//规格/型号/材质
+		 *	        	"goods_number":90,//下单数量
+		 *	        	"send_number":10, //已拆单数量
+		 *	        	"remain_number":80, //未拆单数量
+		 *	        	"goods_price":20, //单价
+		 *	        	"subtotal":1800,//小计
+		 *	        	"suppliers_name":"中交1",//所选供应商
+		 *	        	"add_time":"2015-01-01:11"//下单时间
 		 *	        }
 		 *	        ]
 		 *	    }
@@ -91,19 +115,84 @@ require(dirname(__FILE__) . '/includes/init.php');
 			$order_info_table = $GLOBALS['ecs']->table('order_info');
 			$user_table = $GLOBALS['ecs']->table('users');
 			$contract_table = $GLOBALS['ecs']->table('contract');
-
+			$suppliers_table = $GLOBALS['ecs']->table('suppliers');
 
 			//订单详情
-			$order_sql = 'SELECT odr.`order_status`, odr.`order_sn`, odr.`user_id`, usr.`user_name`, usr.`companyName`, odr.`add_time`, odr.`contract_sn`, crt.`contract_name` FROM ' .
-						 $order_info_table . ' AS odr LEFT JOIN ' . $user_table . '  AS usr ON odr.`user_id` = usr.`user_id` LEFT JOIN ' . $contract_table . ' AS crt ON odr.`contract_sn` = crt.`contract_num` ' . 
+			$order_sql = 'SELECT odr.`order_status`, odr.`order_sn`, odr.`user_id`, usr.`companyName` as `user_name`, odr.`add_time`, odr.`contract_sn`, '. "ifnull(crt.contract_name, '') as contract_name," .//订单相关
+						 ' odr.`consignee`, odr.`address`, odr.`mobile`, odr.`sign_building`, ' .
+					 	 ' odr.`inv_type`, odr.`inv_payee`, odr.`inv_bank_name`, odr.`inv_bank_account`, odr.`inv_bank_address`, odr.`inv_tel`, odr.`inv_fax` ' .	//发票相关
+						 'FROM ' .$order_info_table . ' AS odr LEFT JOIN ' . $user_table . '  AS usr ON odr.`user_id` = usr.`user_id` LEFT JOIN ' . $contract_table . ' AS crt ON odr.`contract_sn` = crt.`contract_num` ' . 
 						 ' WHERE odr.`order_id` = ' . $order_id;
-			$order_info = $GLOBALS['db']->getOne( $order_sql );
+			$order_info = $GLOBALS['db']->getRow( $order_sql );
+
+			if( empty( $order_info ) )
+				make_json_response('', '-1', '订单详情获取失败');
+
+			$order_info['order_id'] = $order_id;
+
+			//发票内容
+			$invoice = array();
+			$invoice['inv_type'] = $order_info['inv_type'];//类型
+			$invoice['inv_payee'] = $order_info['inv_payee'];//公司名称
+			$invoice['inv_bank_name'] = $order_info['inv_bank_name'];//银行
+			$invoice['inv_bank_account'] = $order_info['inv_bank_account'];//帐号
+			$invoice['inv_bank_address'] = $order_info['inv_bank_address'];//银行地址
+			$invoice['inv_tel'] = $order_info['inv_tel'];//联系电话
+			$invoice['inv_fax'] = $order_info['inv_fax'];//联系传真
+
+			unset( $order_info['inv_type'] );
+			unset( $order_info['inv_payee'] );
+			unset( $order_info['inv_bank_name'] );
+			unset( $order_info['inv_bank_account'] );
+			unset( $order_info['inv_bank_address'] );
+			unset( $order_info['inv_tel'] );
+			unset( $order_info['inv_fax'] );
 
 			//物料详情
 			$order_goods_table = $GLOBALS['ecs']->table('order_goods');
-			$order_goods_sql = 'SELECT * FROM ' . $order_goods_table . ' WHERE `order_id` = ' . $order_id;
+			$goods_table = $GLOBALS['ecs']->table('goods');
+			$goods_attr_table = $GLOBALS['ecs']->table('goods_attr');//规格/型号/材质
+
+			// $attribute_table = $GLOBALS['ecs']->table('attribute');
+			$order_goods_sql = 'SELECT og.`goods_id`, og.`goods_name`, og.`goods_sn`, og.`goods_price`, og.`goods_number`, og.`send_number`, sp.`suppliers_name` FROM ' .
+							   $order_goods_table . //物料编码 名称 下单数 已拆 未拆 供应商
+							   ' AS og LEFT JOIN '. $goods_table . ' AS g ON og.`goods_id` = g.`goods_id` ' .
+						 	   'LEFT JOIN ' . $suppliers_table . ' AS sp ON g.`suppliers_id` = sp.`suppliers_id`' . 
+							   ' WHERE `order_id` = ' . $order_id;
 			$order_goods_arr = $GLOBALS['db']->getAll($order_goods_sql);
-							
+
+			if( !empty( $order_goods_arr ) ){
+				//未拆单 小计
+				foreach($order_goods_arr as &$order_good){
+					$order_good['remain_number'] = $order_good['goods_number'] - $order_good['send_number'];//未拆单
+					$order_good['subtotal'] = $order_good['goods_price'] * $order_good['goods_number'];//小计
+					$order_good['add_time'] = date('Y-m-d H:i:s', $order_info['add_time'] );
+
+					//规格、型号、材质
+					$goods_attr_sql = 'SELECT `attr_value` FROM ' . $goods_attr_table .' WHERE `goods_id` = ' . $order_good['goods_id'];
+					$goods_attr = $GLOBALS['db']->getAll( $goods_attr_sql );
+					if( empty( $goods_attr ) ){
+						$order_good['attr'] = '';
+					}else{
+						$attr_arr = array();
+						foreach ($goods_attr as $value) {
+							$attr_arr[] = $value['attr_value'];
+						}
+						$order_good['attr'] = implode('/', $attr_arr);
+					}
+				}
+				unset( $order_info['add_time'] );
+				unset( $order_good );
+			}else{
+				$order_goods_arr = array();
+			}
+				
+			$content = array();
+
+			$content['info'] = $order_info;
+			$content['invoice'] = $invoice;
+			$content['goods'] = $order_goods_arr;
+			make_json_response($content, '0', '订单详情查询成功');
 
 		}
 		
@@ -159,7 +248,7 @@ require(dirname(__FILE__) . '/includes/init.php');
 			$order_table = $GLOBALS['ecs']->table('order_info');
 
 			$sql = 'SELECT odr.`order_id` , odr.`order_sn`, odr.`contract_sn`, odr.`user_id` AS `customer_id`, odr.`add_time`, odr.`best_time`, odr.`goods_amount`, odr.`order_status`' .
-				   ' ,usr.`companyName` as `customer_name`, crt.`contract_name` ' .
+				   ' ,usr.`companyName` as `customer_name`, IFNULL(crt.`contract_name`,\'\') as `contract_name` ' .
 				   ' FROM ' . $order_table .
 				   ' AS odr';
 
@@ -290,11 +379,134 @@ require(dirname(__FILE__) . '/includes/init.php');
 			
 		}
 		
-		
-		public function addInitAction()
+		/**
+		 * 接口名称：子订单拆分初始化
+		 * 接口地址：http://admin.zj.dev/admin/OrderInfoModel.php
+		 * 请求方法：POST
+		 * 传入的接口数据格式如下(具体参数在parameters)：
+	     *  {
+	     *      "entity": "order_info",
+	     *      "command": "splitInit",
+	     *      "parameters": {
+	     *      	"order_id":101,//主订单id
+	     *      	"goods_id":993//商品id
+	     *      }
+	     *  }
+	     * 返回数据格式如下 :
+	     *  {
+		 *		"error": "0",("0": 成功 ,"-1": 失败)
+		 *	    "message": "订单物品拆分信息获取成功",
+		 *	    "content": { 
+		 *	    	"good":
+		 *	        {
+		 *	        	"goods_name":1, //商品名
+		 *              "goods_sn":2 ,//商品编号
+		 *              "goods_id": "xxx" ,//商品id
+		 *              "order_id":101,//订单id
+		 *              "suppliers_id":17,//供应商id
+		 *              "suppliers_name":"上海大汉钢铁",//供应商
+		 *              "attr": "ad11223" //规格/型号/材质
+		 *           },
+		 *	         "order":
+		 *	         {
+		 *	        	"remain_number":80, //未拆单数量
+		 *	        	"goods_price":20, //单价
+		 *	        	"rate":0.008,//金融费率
+		 *	        	"payment":
+		 *	        	[
+		 *	        	{
+		 *	        		"pay_id":1,//支付方式id
+		 *	        		"pay_name":"货到付款"//支付名称
+		 *	        	}
+		 *	        	],
+		 *	         	"suppliers":
+		 *	         	[
+		 *	         	{
+		 *	         		"suppliers_id":17,//供应商ID
+		 *	         		"suppliers_name":"上海大汉钢铁"//供应商名字
+		 *	         	}
+		 *	         	]
+		 *	         }
+		 *	    }
+		 *	}
+		 */
+		public function splitInitAction()
 		{
+			$content = $this->content;
+			$params = $content['parameters'];
+			$goods_id = $params['goods_id'];
+			$order_id = $params['order_id'];
+
+			$goods_table = $GLOBALS['ecs']->table('goods');
+			$payment_table = $GLOBALS['ecs']->table('payment');
+			$contract_table = $GLOBALS['ecs']->table('contract');
+
+			$order_goods_table = $GLOBALS['ecs']->table('order_goods');
+			$goods_attr_table = $GLOBALS['ecs']->table('goods_attr');//规格/型号/材质
+			$suppliers_table = $GLOBALS['ecs']->table('suppliers');
+
+			$order_info_table = $GLOBALS['ecs']->table('order_info');
+
+			//商品详情
+			$good_sql = 'SELECT og.`goods_name`, og.`goods_price`, og.`goods_number`, og.`send_number`, og.`goods_sn`, s.`suppliers_name`, s.`suppliers_id`, ' .
+						' c.`rate` FROM ' . $order_goods_table . 
+						' AS og LEFT JOIN ' . $goods_table . ' AS g ON g.`goods_id` = og.`goods_id` LEFT JOIN ' . $suppliers_table .
+						' AS s ON s.`suppliers_id` = g.`suppliers_id` ' . ' LEFT JOIN ' . $order_info_table .' AS o ON o.`order_id` = og.`order_id` ' .
+						' LEFT JOIN ' . $contract_table . ' AS c ON c.`contract_num` = o.`contract_sn` ' .
+						' WHERE og.`goods_id` = ' . $goods_id . ' AND og.`order_id` = ' . $order_id;
+			$goods = $GLOBALS['db']->getRow( $good_sql );
+
+			if( empty( $goods ) )
+				make_json_response('', '-1', '商品不存在');
+
 			
 
+			//规格、型号、材质
+			$goods_attr_sql = 'SELECT `attr_value` FROM ' . $goods_attr_table .' WHERE `goods_id` = ' . $goods_id;
+			$goods_attr = $GLOBALS['db']->getAll( $goods_attr_sql );
+
+			if( empty( $goods_attr ) ){
+				$goods['attr'] = '';
+			}else{
+				$attr_arr = array();
+				foreach ($goods_attr as $value) {
+					$attr_arr[] = $value['attr_value'];
+				}
+				$goods['attr'] = implode('/', $attr_arr);
+			}
+
+			$goods['remain_number'] = $goods['goods_number'] - $goods['send_number'];//未拆单
+
+			//订单详情
+			//该商品的供应商列表
+			$suppliers_sql = 'SELECT g.`suppliers_id`, s.`suppliers_name` FROM ' . $goods_table . 
+							 ' AS g LEFT JOIN ' . $suppliers_table . ' AS s ON g.`suppliers_id` = s.`suppliers_id` ' .
+							 ' WHERE g.`goods_name` like \'' . $goods['goods_name'] . '\' ';
+			$suppliers = $GLOBALS['db']->getAll( $suppliers_sql );
+			if( empty( $suppliers ) )
+				$suppliers = array();
+
+			//支付方式
+			$payment_sql = 'SELECT `pay_id`, `pay_name` FROM ' . $payment_table;
+			$payment = $GLOBALS['db']->getAll( $payment_sql );
+			if( empty( $payment ) )
+				$payment = array();
+
+			$order['suppliers'] = $suppliers;
+			$order['payment'] = $payment;
+			$order['remain_number'] = $goods['remain_number'];
+			$order['goods_price'] = $goods['goods_price'];
+			$order['rate'] = $goods['rate'];
+			
+			unset($goods['remain_number']);
+			unset($goods['goods_price']);
+			unset($goods['rate']);
+
+			$content = array();
+			$content['good'] = $goods;
+			$content['order'] = $order;
+
+			make_json_response( $content, '0', '商品拆分初始化成功' );
 		}
 
 		
@@ -306,6 +518,6 @@ require(dirname(__FILE__) . '/includes/init.php');
 			
 		
 	}
-	$content = jsonAction( array( "editInit", "addInit" ) );
+	$content = jsonAction( array( "splitInit", "editInit" ) );
 	$orderModel = new OrderInfoModel($content);
 	$orderModel->run();
