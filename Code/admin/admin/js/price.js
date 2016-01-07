e4d1c00c5066d6bc8340ed4bcb787465ec3e3cf8
xiangfeng <fxiang@3ti.us>
2016-01-07 18:25:31 +0800

    update

--- Code/admin/admin/js/price.js
+++ Code/admin/admin/js/price.js
@@ -1,5 +1,6 @@
 var Price = {
 	order_arr: [
+
 	],
 	limit: 0,
 	offset: 8,
@@ -8,9 +9,36 @@ var Price = {
 	entity: "price_adjust",
 
 	getList: function(){
-		var params = {"params":{"limit":this.limit, "offset":this.offset}};
-		strJson = createJson("page", this.entity, params);
+		if(typeof(search) === "undefined"){
+			var params = {"params":{"limit":this.limit, "offset":this.offset}};
+		}else{
+			var condition = {};
+			var search_type = $('#contract_search_form select[name=search_type]').val();
+			var search_value = $('#contract_search_form input[name=search_value]').val();
+			var contract_status = $('#contract_search_form select[name=contract_status]').val();
+			var start_time = $('#contract_search_form input[name=start_time]').val();
+			var end_time = $('#contract_search_form input[name=end_time]').val();
+			if(search_value != ''){
+				condition.like = {"search_type":search_type, "search_value":search_value};
+			}
+			if(contract_status != ''){
+				condition.contract_status = contract_status;
+			}
+			if(start_time != ''){
+				condition.start_time = start_time;
+			}
+			if(end_time != ''){
+				condition.end_time = end_time;
+			}
+			if(search == "search"){
+				this.current_page = 1;
+				this.limit = 0;
+			}
+			var params = {"params":{"where":condition, "limit":this.limit, "offset":this.offset}};
+		}
+		strJson = createJson("priceList", this.entity, params);
 		that = this
+		console.log(strJson);
 		$.post(this.url, strJson, function(obj){
 			if(obj.error == -1){
 				$('#message_area').html(createError(obj.message));
@@ -37,7 +65,6 @@ var Price = {
 				$('#message_area').html(createError(obj.message));
 				return false;
 			}else{
-				console.log(obj)
 				var row = '';
 				$.each(obj.content, function(k,v){
 					row += appendOption(v.brand_id, v.brand_name);
