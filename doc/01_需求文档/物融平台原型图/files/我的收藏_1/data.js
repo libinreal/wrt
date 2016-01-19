a16545a9fd92756dc5b6e9a9e5ecafd18a3ad869
niu <280850589@qq.com>
2016-01-19 18:40:36 +0800

    新增加物融平台原型图-全

---
+++ doc/01_需求文档/物融平台原型图/files/我的收藏_1/data.js
@@ -0,0 +1,71 @@
+﻿$axure.loadCurrentPage({
+  "url":"我的收藏_1.html",
+  "generationDate":new Date(1453199923885.07),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"9d0718497f734f849e64ff9ac1fb607d",
+    "type":"Axure:Page",
+    "name":"我的收藏",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[{
+          "id":"5d14c46944214b46b8690db3ee705bd7",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":20,
+              "y":10},
+            "size":{
+              "width":912,
+              "height":108}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"617a91c282fb4eaa864c1c9b779f0942",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":20,
+                  "y":10},
+                "size":{
+                  "width":912,
+                  "height":108}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/我的收藏/u0.png"}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "5d14c46944214b46b8690db3ee705bd7":{
+      "scriptId":"u0"},
+    "617a91c282fb4eaa864c1c9b779f0942":{
+      "scriptId":"u1"}}});
