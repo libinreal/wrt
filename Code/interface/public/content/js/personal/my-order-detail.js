f53b98ba307d585fa79b79b404cfc6395da3c925
xiangfeng <fxiang@3ti.us>
2016-02-01 15:44:06 +0800

    update

--- Code/interface/public/content/js/personal/my-order-detail.js
+++ Code/interface/public/content/js/personal/my-order-detail.js
@@ -260,7 +260,7 @@ define(function(require) {
     });
 
     // 
-    $('#handle-button').on('click', '.button-check' function(e) {
+    $('#handle-button').on('click', '.button-check', function(e) {
         var oid = $("#handle-button-check").attr("data-id");
         var data = {"oid":oid};
         $.get("order/uchildstatus", data, function(object){
