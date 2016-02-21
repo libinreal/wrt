60305d6812f0018c3c0931afca1e79b3cceff1aa
libin <libin@3ti.us>
2016-02-21 18:46:36 +0800

    采购到货改价

--- Code/admin/admin/PurchaseOrderModel.php
+++ Code/admin/admin/PurchaseOrderModel.php
@@ -1288,9 +1288,9 @@ require(dirname(__FILE__) . '/includes/init.php');
 			if( !isset( $params['order_id'] ) ){
 				make_json_response('', '-1', '订单ID错误');
 			}
-			if( !isset( $params['suppers_id'] ) ){
+			/*if( !isset( $params['suppers_id'] ) ){
 				make_json_response('', '-1', '请选择供应商');
-			}
+			}*/
 			$order_id = intval( $params['order_id'] );
 
 			$order_info_table = $GLOBALS['ecs']->table('order_info');
@@ -1372,7 +1372,7 @@ require(dirname(__FILE__) . '/includes/init.php');
 		}
 
 
-		
+
 
 
 	}
