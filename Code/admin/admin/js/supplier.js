f477ef171efcfe3b790e2c801dad5fc031b3b9bc
xiangfeng <fxiang@3ti.us>
2016-02-23 11:41:49 +0800

    供应商已完成订单列表

--- Code/admin/admin/js/supplier.js
+++ Code/admin/admin/js/supplier.js
@@ -16,6 +16,17 @@ var Supplier = {
 		"desc",
 		"operate"
 	],
+	complete_arr: [
+		"order_sn",
+		"goods_name",
+		"goods_sn",
+		"attr",
+		"goods_price",
+		"goods_number",
+		"shipping_fee",
+		"purchase_pay_status"
+	],
+	purchase_pay_status: {},
 	limit: 0,
 	offset: 8,
 	total_page: 0,
@@ -451,5 +462,73 @@ var Supplier = {
 			}
 			$('#message_area').html('');
 		}, "json");
+	},
+
+	getCompleteList: function(search){
+		if(search == undefined){
+			serach = false;
+		}else{
+			var condition = {};
+			var status = $('#search_form select[name=status]').val();
+			var due_date1 = $('#search_form input[name=due_date1]').val();
+			var due_date2 = $('#search_form input[name=due_date2]').val();
+			if(status != ''){
+				condition.status = status;
+			}
+			if(due_date1 != ''){
+				condition.due_date1 = due_date1;
+			}
+			if(due_date2 != ''){
+				condition.due_date2 = due_date2;
+			}
+		}
+		if(search != false){
+			if(search == "search"){
+				this.limit = 0;
+			}
+			var params = {"params":{"where":condition,"limit":this.limit, "offset":this.offset}};
+		}else{
+			var params = {"params":{"limit":this.limit, "offset":this.offset}};
+		}
+		var strJson = createJson("completeList", this.entity, params);
+		var that = this
+		$.post(this.url, strJson, function(obj){
+			if(obj.error == -1){
+				$('#message_area').html(createError(obj.message));
+				return false;
+			}else{
+				that.total_page = Math.ceil(obj.content.total/that.offset);
+				if(obj.content.total == 0){
+					var row = "<tr><td colspan='20'>"+createWarn("无数据")+"</td></tr>";
+					$("#main_list>tbody").html(row);
+					$("#paginate").html('');
+				}else{
+					$("#paginate").html(createPaginate(that.url, obj.content.total, that.current_page, that.limit, that.offset));
+					var row = "";
+					$.each(obj.content.data,function(key, value){
+						row += "<tr>";
+						for(var i=0;i<that.complete_arr.length;i++){
+							if(that.complete_arr[i] == "operate"){
+								var edit = createLink("demo_template.php?section=supplier&act=order_detail&id="+value.order_id, "详情");
+								row += createTd(edit);
+								continue;
+							}
+							if(that.complete_arr[i] == "purchase_pay_status"){
+								row += createTd(that.purchase_pay_status[value.purchase_pay_status]);
+								continue;
+							}
+							if(value[that.complete_arr[i]] != null){
+								row += createTd(subString(value[that.complete_arr[i]],18,true));
+							}else{
+								row += createTd(createWarn('无数据'));
+							}
+						}
+						row += "</tr>";
+						$("#main_list>tbody").html(row);
+					});
+				}
+			}
+			$('#message_area').html('');
+		}, "json");		
 	}
 }
